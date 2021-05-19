4019dee6d2041b2d5a8cf841f7f202fcd648dd88
Jahid <jahid90@users.noreply.github.com>
2021-05-20 02:04:33 +0530

    move views; import ids and layouts; update call to layout service to use post

---
+++ src/pages.js
@@ -0,0 +1,10 @@
+module.exports = {
+    identifiers: {
+        '/': 1,
+        '/about': 2
+    },
+    layouts: {
+        'single-column-with-nav': 'home'
+    }
+};
+
--- src/server.js
+++ src/server.js
@@ -1,23 +1,28 @@
 const axios = require('axios');
 const express = require('express');
+const path = require('path');
+
+const { identifiers, layouts } = require('./pages');
 
 // Create the server
 const app = express();
 
 // Configurations
+app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'pug');
-
-const pages = {
-    'single-column-with-nav': 'home'
-};
 
 // Routes
 app.get('/', async (req, res) => {
     
-    const content = await axios.get('http://layout.service');
+    const pageId = identifiers['/'];
+    const content = await axios.post('http://layout.service', { pageId });
     const { title, layout, slots } = content.data.page;
 
-    res.render(pages[layout], { title, slots });
+    res.render(layouts[layout], { title, slots });
+});
+
+app.get('/ping', (req, res) => {
+    res.send('OK');
 });
 
 // Start the server
