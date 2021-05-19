212058501ccd76a5047e8cd011d045a2a690d45b
Jahid <jahid90@users.noreply.github.com>
2021-05-20 00:58:25 +0530

    pull page metadata from layout service

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
+const axios = require('axios');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('This is the page service!');
+app.get('/', async (req, res) => {
+    const layoutContent = await axios.get('http://layout.service');
+
+    res.send(`
+        This is the page service!
+
+        I pulled the following content fror the layout
+
+        ${JSON.stringify(layoutContent.data, null, 2)}
+    `);
 });
 
 const port = process.env.PORT || 3000;
